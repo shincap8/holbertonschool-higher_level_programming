7c4ea01381e13367e056b5c05d6b759afca95b61
shincap8 <shincap8@gmail.com>
2019-12-10 19:16:50 +0000

    Tasks 8 to 12

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        upper = ord(i);
+        if upper >= 97 and upper <= 122:
+            upper = upper - 32
+        print('{:c}'.format(upper), end = "")
+    print("")
