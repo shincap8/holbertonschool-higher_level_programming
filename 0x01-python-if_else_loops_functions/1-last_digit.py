9452f187cd9443e4a27ec6c68dd72a49a198387c
shincap8 <shincap8@gmail.com>
2019-12-10 17:37:21 +0000

    Correct requirement line too long

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 text = "Last digit of"
 if number < 0:
-    last_digit = ((number * -1) % 10) * -1
+    last_d = ((number * -1) % 10) * -1
 else:
-    last_digit = number % 10
-if last_digit > 5:
-    print('{} {} is {} and is greater than 5'.format(text, number, last_digit))
-elif last_digit == 0:
-    print('{} {} is {} and is 0'.format(text, number, last_digit))
+    last_d = number % 10
+if last_d > 5:
+    print(text, number, ' is ', last_d, 'and is greater than 5')
+elif last_d == 0:
+    print(text, number, ' is ', last_d, 'and is 0')
 else:
-    print('{} {} is {} and is less than 6 and not 0'.format(text, number, last_digit))
+    print(text, number, ' is ', last_d, 'and is less than 6 and not 0')
