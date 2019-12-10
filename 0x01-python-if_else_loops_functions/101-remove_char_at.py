aefd1e7efcbdaa526f135cd14703f5aebaa510ea
shincap8 <shincap8@gmail.com>
2019-12-10 22:24:08 +0000

    second advance task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/uesr/bin/python3
+def remove_char_at(str, n):
+    if n < 0:
+        return(str)
+    text = ""
+    for i in range(0, len(str)):
+        if i != n:
+            text = text + str[i];
+    return(text)
