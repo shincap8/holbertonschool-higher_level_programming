9c3fb235f8e6749c50acf99bb123a405c068ee58
shincap8 <shincap8@gmail.com>
2019-12-12 21:10:50 +0000

    Correct pep8 task8 remove space before parenthesis

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    for i in range (len(dir(hidden_4))):
+    for i in range(len(dir(hidden_4))):
         if dir(hidden_4)[i][0] != '_' and dir(hidden_4)[i][1] != '_':
-    	    print("{}".format(dir(hidden_4)[i]))
+            print("{}".format(dir(hidden_4)[i]))
