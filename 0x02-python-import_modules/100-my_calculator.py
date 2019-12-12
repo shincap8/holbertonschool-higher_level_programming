8d8fc1cfe4527dd493150c71145bd97e982c5718
shincap8 <shincap8@gmail.com>
2019-12-12 21:52:05 +0000

    Correct requirement first advance

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-import sys
 if __name__ == '__main__':
+    import sys
     from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         sys.exit(1)
     operator = sys.argv[2]
     if operator == "+":
-        print(add(sys.argv[1], sys.argv[3]))
+        print(add(int(sys.argv[1]), int(sys.argv[3])))
     elif operator == "-":
-        print(sub(sys.argv[1], sys.argv[3]))
+        print(sub(int(sys.argv[1]), int(sys.argv[3])))
     elif operator == "*":
-        print(mul(sys.argv[1], sys.argv[3]))
+        print(int(mul(sys.argv[1]), int(sys.argv[3])))
     elif operator == "/":
-        print(div(sys.argv[1], sys.argv[3]))
+        print(div(int(sys.argv[1]), int(sys.argv[3])))
     else:
         print("Unknown operator. Available operators: +, -, * and /i")
         sys.exit(1)
