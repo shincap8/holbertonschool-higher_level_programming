333483bc7461c771ab33063740bc9d0b0f8be74d
shincap8 <shincap8@gmail.com>
2019-12-17 17:20:56 +0000

    Task 0 to 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+* is_palindrome- checks if a list is palindrom
+* @head: double pointer to the first element
+*
+* Description: this function prints all elements of a list
+* Return: 1 if the list is palindrome 0 if not
+*/
+int is_palindrome(listint_t **head)
+{
+	size_t nodes = 0;
+	unsigned int i, x;
+	listint_t *tmp = *head, *backwards;
+
+	if (*head == NULL || head = NULL)
+		return(1);
+	while (tmp)
+	{
+		tmp = tmp->next;
+		nodes++;
+	}
+	if ((nodes % 2) == 0)
+		x = 
+	tmp = *head;
+	while ()
+	return (nodes);
+}
