e317134991707b5eb9f837040a3f4c667ec82007
shincap8 <shincap8@gmail.com>
2019-12-18 01:24:18 +0000

    Remove pycache andcorrect output last task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,24 +8,24 @@
 */
 int is_palindrome(listint_t **head)
 {
-	size_t nodes = 0;
-	unsigned int i = 1, j, x;
-	int tmp[1000] ;
+        size_t nodes = 0;
+        unsigned int i = 1, j, x;
+        int tmp[1000] ;
 
-	if (*head == NULL || head == NULL)
-		return (1);
-	while (*head)
-	{
-		tmp[nodes] = *head->n;
-		*head = *head->next;
-		nodes++;
-	}
-	x = nodes / 2;
-	for (i = 1, i <= x, i++)
-	{
-		j = nodes - i;
-		if (tmp[i] != tmp[j])
-			return (0);
-	}
-	return (1);
+        if (*head == NULL || head == NULL)
+                return (1);
+        while (*head)
+        {
+                tmp[nodes] = (*head)->n;
+                *head = (*head)->next;
+                nodes++;
+        }
+        x = nodes / 2;
+        for (i = 0; i <= x; i++)
+        {
+                j = nodes - i - 1;
+                if (tmp[i] != tmp[j])
+                        return (0);
+        }
+        return (1);
 }
