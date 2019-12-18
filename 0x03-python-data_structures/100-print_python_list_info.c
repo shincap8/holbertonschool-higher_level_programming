e46d12556e0050a77dc55dce3cb6dba4190e1ed5
shincap8 <shincap8@gmail.com>
2019-12-18 17:56:45 +0000

    Correct compile advance task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
 #include <Python.h>
+#include <stdio.h>
 
 /**
 * print_python_list_info - prints info about python list
 * @p: list pointer
 *
 * Return: void
-**/
+*/
 void print_python_list_info(PyObject *p)
 {
-	int i, length;
+	int i, len;
 	Pyobject *element;
 
-	length = Pylist_Size(p);
+	len = Pylist_Size(p);
 
-	printf("[*] Size of the Python List = %d\n", length);
+	printf("[*] Size of the Python List = %d\n", len);
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (i = 0; i < length; i++)
+	for (i = 0; i < len; i++)
 	{
 		element = PyList_GetItem(p, i);
 		printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name);
