e856618288fa4bc522920a737eb54358fd1d57e4
shincap8 <shincap8@gmail.com>
2019-12-20 03:49:20 +0000

    First advance

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    divisor = 0
+    mul = 1
+    dividend = 0
+    flag = 0
+    for i in my_list:
+        for j in i:
+            mul = mul * j
+            if flag == 1:
+                flag = 0
+                divisor = divisor + j
+            else:
+                flag = 1
+        dividend = dividend + mul
+        mul = 1
+    return (dividend / divisor)
