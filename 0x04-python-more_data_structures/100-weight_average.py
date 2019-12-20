a4659b860512cd610521f7cdaa23aa6cc5e25490
shincap8 <shincap8@gmail.com>
2019-12-20 03:51:56 +0000

    Correct empty list condition

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,14 +4,17 @@ def weight_average(my_list=[]):
     mul = 1
     dividend = 0
     flag = 0
-    for i in my_list:
-        for j in i:
-            mul = mul * j
-            if flag == 1:
-                flag = 0
-                divisor = divisor + j
-            else:
-                flag = 1
-        dividend = dividend + mul
-        mul = 1
-    return (dividend / divisor)
+    if my_list:
+        for i in my_list:
+            for j in i:
+                mul = mul * j
+                if flag == 1:
+                    flag = 0
+                    divisor = divisor + j
+                else:
+                    flag = 1
+            dividend = dividend + mul
+            mul = 1
+        return (dividend / divisor)
+    else:
+        return 0
