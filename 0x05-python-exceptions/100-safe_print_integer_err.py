6b4bf55effa7bb8a418e7ea61dc6c26cf951eaa6
shincap8 <shincap8@gmail.com>
2020-01-08 23:20:35 +0000

    requirement task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except Exception as err:
         print ("Exception: {}".format(err), file=sys.stderr)
         return False
+    return True
