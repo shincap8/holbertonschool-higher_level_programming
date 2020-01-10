deeed27ca15698ddab80442f53a0fe76eeba57d5
shincap8 <shincap8@gmail.com>
2020-01-10 18:24:06 +0000

    Adding comments and changing or condition

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 import math
+"""Import math"""
 
 
 class MagicClass:
+    """Magic Class"""
     def __init__(self, radius=0):
-        if type(radius) is not int and type(radius) is not float:
+        if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
-            return self.__radius = None
         else:
             self.__radius = radius
 
     def area(self):
+    """Area method"""
         return ((self ** 2) * math.pi)
 
         def circumference:
+            """Circumference method"""
             return ((math.pi * 2) * self__radius)
