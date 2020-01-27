4901dc1a7dba3d743d17492bb95cb65442f1a297
shincap8 <shincap8@gmail.com>
2020-01-27 19:35:19 +0000

    return empty list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base:
     def from_json_string(json_string):
         """comment"""
         if json_string is None or len(json_string) == 0:
-            return ("[]")
+            return ([])
         return (json.loads(json_string))
 
     @classmethod
