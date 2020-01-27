8884402c422cc9f70de829e0440f492ad0c1bf83
shincap8 <shincap8@gmail.com>
2020-01-27 18:36:13 +0000

    Change the order of the if statement

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """commment"""
-        if len(list_dictionaries) <= 0 or list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
