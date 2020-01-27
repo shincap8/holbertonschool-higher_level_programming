9848ab69a15bd0289fcb871d474336f5235da04d
shincap8 <shincap8@gmail.com>
2020-01-27 22:41:41 +0000

    fucking checker

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,6 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """comment"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         return json.dumps(list_dictionaries)
