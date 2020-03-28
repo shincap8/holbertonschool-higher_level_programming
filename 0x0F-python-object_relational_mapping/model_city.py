0800f38e1dcfa9b72c3dfcee28167a3193a0ea9d
shincap8 <shincap8@gmail.com>
2020-03-28 22:47:32 +0000

    Task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""python file that contains the class definition of a
+city and an instance Base = declarative_base():"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """City Class"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    def __repr__(self):
+        """function"""
+        return "<City(name='%s')>" % self.email_address
