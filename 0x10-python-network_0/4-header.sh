cb4217cf6f1ec9f99cfea8203ba9fdd64709f559
shincap8 <shincap8@gmail.com>
2020-04-09 18:20:54 +0000

    Use of command prompt

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s 0.0.0.0:5000/route_5 -H "X-HolbertonSchool-User-Id: 98"
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
