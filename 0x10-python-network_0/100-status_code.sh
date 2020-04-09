4f5c9798148e12555d37797c1d7d36995274fa74
shincap8 <shincap8@gmail.com>
2020-04-09 18:58:04 +0000

    remove pipe lines

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI "$1" | grep Content-Length | cut -d " " -f 2
+curl -so /tmp/null -sIw %{http_code} "$1
