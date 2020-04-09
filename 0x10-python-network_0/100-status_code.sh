2d5049bb8c0d64dbc3d922633b361d2a7c8527d6
shincap8 <shincap8@gmail.com>
2020-04-09 18:48:33 +0000

    Advance task

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -so dev/null/ -w %{http_code} "$1"
+curl -so /tmp/null/ -swl %{http_code} "$1"
