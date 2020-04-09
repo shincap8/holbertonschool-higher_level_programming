51713b64391703fb0e2cfcf0ddb544f56f871903
shincap8 <shincap8@gmail.com>
2020-04-09 18:51:47 +0000

    some changes add last line

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -so /tmp/null/ -swl %{http_code} "$1"
+curl -sI "$1" | grep Content-Length | cut -d " " -f 2
