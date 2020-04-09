60a9586241e2df64b55196415c971ec9209c4c39
shincap8 <shincap8@gmail.com>
2020-04-09 19:04:12 +0000

    add missing double quote

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -so /tmp/null -sIw %{http_code} "$1
+curl -so /tmp/null -sIw %{http_code} "$1"
