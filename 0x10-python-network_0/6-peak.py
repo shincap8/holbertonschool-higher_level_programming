dba20b4c89f15c31b69ce963d3a50680b186a8cd
shincap8 <shincap8@gmail.com>
2020-04-10 20:53:12 +0000

    Order list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,23 +4,8 @@
 
 def find_peak(list_of_integers):
     """function to find the peak"""
-    length = len(list_of_integers)
-    if length > 1:
-        middle = int(length / 2)
-        for i in range(middle):
-            if i == 0 and list_of_integers[i] >= list_of_integers[i + 1]:
-                return list_of_integers[i]
-            if list_of_integers[i] >= list_of_integers[i + 1] and\
-            list_of_integers[i] >= list_of_integers[i - 1]:
-                return list_of_integers[i]
-        for i in range(middle, length):
-            if i == (length - 1) and list_of_integers[i] >\
-            list_of_integers[i - 1]:
-                return list_of_integers[i]
-            if list_of_integers[i] >= list_of_integers[i + 1] and\
-            list_of_integers[i] >= list_of_integers[i - 1]:
-                return list_of_integers[i]
-    if length == 1:
-        return list_of_integers[0]
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
     else:
-        return None
+         return None
