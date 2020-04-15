473dff0ab25b08b2f93b31bfdb92a04923cb7222
shincap8 <shincap8@gmail.com>
2020-04-15 19:57:51 +0000

    Remove if main

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,7 @@
 from urllib import request
 
 
-if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
-        format(type(html), html, html.decode("utf-8")))
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
+      format(type(html), html, html.decode("utf-8")))
