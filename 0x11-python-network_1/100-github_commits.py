eb812648a9dda7c659935385ce084e4b1532e33f
shincap8 <shincap8@gmail.com>
2020-04-16 15:29:57 +0000

    First advance

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that takes your Github credentials (username and password)
+and uses the Github API to display your id"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/"+argv[1]+"/"+argv[2]+"/commits"
+    r = requests.get(url)
+    commits = r.json()
+    for i in range(10):
+        sha = commits[i]["sha"]
+        name = commits[i]["commit"]["author"]["name"]
+        print("{}: {}".format(sha, name))
