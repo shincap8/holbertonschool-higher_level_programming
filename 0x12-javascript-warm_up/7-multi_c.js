350f82c41c181cb3a432785ecd4e4d9380ac7457
shincap8 <shincap8@gmail.com>
2020-05-05 17:22:03 +0000

    C in capital letter

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const x = process.argv.slice(2);
 if (parseInt(x[0])) {
   for (let i = 0; i < x; i++) {
-    console.log('c is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
