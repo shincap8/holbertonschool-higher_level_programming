2b0134948463e32f1729abdec50e20912c433545
shincap8 <shincap8@gmail.com>
2020-05-12 20:00:21 +0000

    Just ask for the 18/ in if

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,15 +2,16 @@
 const myArgs = process.argv.slice(2);
 const request = require('request');
 let count = 0;
-const character = 'https://swapi-api.hbtn.io/api/people/18/';
 request(myArgs[0], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const list = JSON.parse(body).results;
     for (const i of list) {
-      if (i.characters.includes(character)) {
-        count++;
+      for (const j of i.characters) {
+        if (j.slice(-3) === '18/') {
+          count++;
+        }
       }
     }
     console.log(count);
