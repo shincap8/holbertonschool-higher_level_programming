cda5fb5e8faa9b965662c56f56f0555a9297d1bf
shincap8 <shincap8@gmail.com>
2020-05-12 18:47:52 +0000

    Change in the console log

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const myArgs = process.argv.slice(2);
 const request = require('request');
 request.get(myArgs[0]).on('response', function (response) {
-  console.log(response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
