e7e861bea41d19b778e530ec224e0797915666c2
shincap8 <shincap8@gmail.com>
2020-05-12 21:58:52 +0000

    Remove console-log to debug

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,8 @@ const myArgs = process.argv.slice(2);
 const reqURL = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
 const request = require('request');
 request(reqURL, function (error, response, body) {
-  console.log(response.statusCode);
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     console.log(JSON.parse(body).title);
   }
